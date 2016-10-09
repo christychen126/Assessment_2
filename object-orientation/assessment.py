a13c074833da81e84ddb610283e87bfab9750193
Christy Chen <christychen126@gmail.com>
2016-10-08 23:10:29 -0700

    OOP discussion done

--- object-orientation/assessment.py
+++ object-orientation/assessment.py
@@ -3,17 +3,40 @@ Part 1: Discussion
 
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
+    1. Abstraction: It hides details from users, but user can still use it without 
+        knowing how it works. 
+    2. Encapsulation: 
+        Objects in the same class will have similar properties, and methods are close 
+        to the data(objects) that they can be called on.
+    3. Polymorphism: For children classes that inherit from same parent class, 
+        there are some commons in the children classes. They can have same attributes 
+        or can call same methods which are defined in their parent class. 
 
 2. What is a class?
+    A class can group functions(methods) and attributes which can be applied to 
+    some objects (intances) together.  
 
 3. What is an instance attribute?
+    It's a variable in a class and is specific to each instance.
 
 4. What is a method?
+    It is a functions that is defined in a class, and can only apply to instances 
+    belong to the class. It has to call "self" as it's first argument.
+
 
 5. What is an instance in object orientation?
+    It is an object that is initiated in a class. It has it's own data, and can call
+    methods from the class that it is initiated in. 
 
 6. How is a class attribute different than an instance attribute?
    Give an example of when you might use each.
+   A class attribue is a variable definded at class level, while an instance attribute 
+   is defined at instance level and will vary when differnt instances are initiated.
+   Instances in the same class will have same class attributes, but differnt instance
+   attributes.
+   For example, "is drinkable" can be a class attribute for a class of "Coffee", 
+   but each instance of "Coffee" can have differnt types, names, origins, tastes 
+   which are instance attributes. 
 
 
 """
